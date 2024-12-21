8d98d471afb140e124703d8d26759bed49868a79
Junior Amorim <junioramorim144@gmail.com>
2024-12-20 22:06:21 -0300

    pegando o jeito

--- python-testes/testes.py
+++ python-testes/testes.py
@@ -1,2 +1,10 @@
-print('Olá, Mundão!!
-')
+print('Olá, Mundão!!')
+print('quero que vocês digitem um número!')
+
+n1 = int (input ('digite um número'))
+
+n2 = int (input ('digite o segundo número'))
+
+res = (n1 + n2)
+
+print('A soma entre {} e {} é {}' .format (n1,n2,res))
