dc95736293bb3b85201bdfd8cf3fb8eba80ade7e
Junior Amorim <junioramorim144@gmail.com>
2025-01-05 23:50:32 -0300

    estudo

---
+++ python-testes/teste-05.py
@@ -0,0 +1,6 @@
+nota1 = float (input('Digite a primeira nota'))
+nota2 = float (input ('Digite o segundo número'))
+
+f = (nota1 + nota2) /2
+
+print((f'O aluno tirou {nota1} e {nota2} logo então sua média é {f}.'))
