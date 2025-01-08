bfb19ad3b5f460438afa1d383905a442f041ec17
Junior Amorim <junioramorim144@gmail.com>
2025-01-07 23:21:04 -0300

    estudo

---
+++ python-testes/teste-06.py
@@ -0,0 +1,4 @@
+print('Conversor de real para dolar')
+real = float (input('Quanto de Dinheiro você tem na carteira R$?'))
+res = real / 6.10
+print(f'com R${real:.2f} Você compra U${res:.2f} ')
---
+++ python-testes/teste-07.py
@@ -0,0 +1,5 @@
+print('Dimmensões da parede')
+altura = float(input('Digite a altura da sua parede.'))
+largura = float(input('Digite a largura da sua parede'))
+mqua = altura * largura
+print(f'A altura da sua parede é {altura} a largura é {largura} logo então ela tem {mqua} metros quadrados') 
