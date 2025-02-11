5a4b92c09760c162d38459ea519849353c329ed6
Junior Amorim <junioramorim144@gmail.com>
2025-02-11 13:00:32 -0300

    estudo

--- python-testes/teste-13.py
+++ python-testes/teste-13.py
@@ -1,11 +1,11 @@
-import random
+'''import random
 n1 = str (input('Qual é o primeiro nome:'))
 n2 = str (input('Qual é o Segundo  nome:'))
 n3 = str (input('Qual é o terceiro nome:'))
 n4 = str (input('Qual é o quarto nome:'))
 lista = [n1, n2, n3, n4]
 escolhido = random.choice(lista)
-print (f'O aluno escolhido foi {escolhido}')
+print (f'O aluno escolhido foi {escolhido}')'''
 
 #ou
 
@@ -18,3 +18,17 @@ print (f'O aluno escolhido foi {escolhido}')
 #lista = [n1, n2, n3, n4]
 #escolhido = choice(lista)
 #print (f'O aluno escolhido foi {escolhido}')
+
+
+#import random 
+from random import shuffle 
+
+n1 = str (input('Digite o primeiro nome:'))
+n2 = str (input('Digite o segundo nome:'))
+n3 = str (input('Digite p terceiro nome:'))
+n4 = str (input ('Digite o quarto nome:'))
+
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print('A Ordem de apresentação será:')
+print(lista)
