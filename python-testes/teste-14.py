92d14982520f69e1700684bcd6ff53a598b53213
Junior Amorim <junioramorim144@gmail.com>
2025-02-14 18:32:56 -0300

    estudo

--- python-testes/teste-14.py
+++ python-testes/teste-14.py
@@ -1,14 +1,14 @@
 print('Olá, tudo bem? preciso que você digite seu nome todo abaixo:')
-nome = input('Digite seu nome:')
-print(f'Olá {nome} segue as características abaixo')
+nome = input('Digite seu nome:').strip() 
+print(f'Olá {nome}, segue as características abaixo')
 
 
-print(nome.strip())
-print(f'seu nome tem {len(nome)} letras')
+
+print('Seu nome tem {} letras'.format(len(nome)- nome.count(' ')))
 primeiro_nome = nome.split()[0]
-print(len(f'Seu primeiro nome tem {primeiro_nome} letras'))
+print(f'Seu primeiro nome tem {len(primeiro_nome)} letras')
 print(f'Seu nome maiúsculo {nome.upper()}')
-print(f'Seu nome minusculo {nome.lower()} ')
+print(f'Seu nome minusculo {nome.lower()}')
 
 
 
