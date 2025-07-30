a96daefef8615dcc59f422de5ca172e62d031dd4
Junior Amorim <junioramorim144@gmail.com>
2025-07-29 23:03:49 -0300

    correção

--- python-testes-m1/teste-09.py
+++ python-testes-m1/teste-09.py
@@ -1,3 +1,3 @@
 valor = float (input ('Digite sua renda mensal para saber seu aumento:'))
 aumento = valor + (valor * 15 / 100)
-print(f'Você temm uma renda de R${valor:.2f} Com o aumento de 15% seu renda vai para R${aumento:.2f}')
+print(f'Você tem uma renda de R${valor:.2f} Com o aumento de 15% seu renda vai para R${aumento:.2f}')
