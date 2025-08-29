1d6a817e1f6125722279f8fbfeee069c8efef9a0
Junior Amorim <junioramorim144@gmail.com>
2025-08-28 23:19:43 -0300

    a volta

---
+++ python-mundo-2/exe-43.py
@@ -0,0 +1,11 @@
+soma = 0 
+ac = 0
+for r in range(1,7):
+    num = int(input('Digite um valor'))
+    if num % 2 == 0:
+        soma = soma + num
+        ac = ac + 1
+print(f'O valor digitado foram {ac} vezes e a soma é {soma} números')
+#volta dos estudos 28/08/25
+#Fiz sozinho
+
