e7606fd8a8ab35d1e2c3ca736b05fdc1c6109f76
Junior Amorim <junioramorim144@gmail.com>
2025-09-06 09:12:00 -0300

    estudo

--- python-mundo-2/exe-50.py
+++ python-mundo-2/exe-50.py
@@ -1,4 +1,4 @@
 sexo = str (input('Digite seu sexo: [M/F]')).strip().upper()[0]
 while sexo not in 'MF':
-    sexo = str(input('Dados inválidos. Por favor, informe seu sexo'))
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo')).strip().upper()[0]
 print(f'Seu sexo escolhido é {sexo}.')
--- python-mundo-2/exe-52.py
+++ python-mundo-2/exe-52.py
@@ -1,4 +1,14 @@
 n1 = int(input('Digite o primeiro valor:'))
 n2 = int(input('Digite o segundo valor: '))
+total = 0
 numero = int(input('[1] somar [2] multiplicar [3] maior [4] novos números [5] sair do programa'))
-#while 
+while numero == 5:
+    if numero == 1:
+        total += n1 + n2
+    if numero == multiplicar:
+        total += n1*n2
+        if  n1 > n2:
+            
+
+
+print('O programa fechou!')
