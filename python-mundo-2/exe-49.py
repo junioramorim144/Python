a565d4e0782ece810b172b7087e3553ce10d4748
Junior Amorim <junioramorim144@gmail.com>
2025-09-06 16:21:46 -0300

    06/09

--- python-mundo-2/exe-49.py
+++ python-mundo-2/exe-49.py
@@ -2,7 +2,8 @@ mediaidade = 0
 somaidade = 0
 quantmulheres = 0
 homemvelho = 0 
-maisvelho = 0    
+maisvelho = 0 
+maisidade = 0   
 for c in range( 1,5):
     print(f'{'-'*5}{c}ª Pessoa{'-'*5}')
     nome = str(input('Digite o nome.')).strip()
@@ -16,13 +17,13 @@ for c in range( 1,5):
         maisidade = idade
         homemvelho = nome 
     if sexo in 'fF' and idade < 20:
-        quantmulheres + 1 
+        quantmulheres += 1 
 mediaidade = somaidade/4
 #print(mediaidade)
 
 print(f'A média de idade é {mediaidade}')
 print(f'O nome do homem mais velho é {homemvelho} com {maisidade} de idade.')
-print('A quantidade de mulher com menos de 20 é {quantmulheres}')         
+print(f'A quantidade de mulher com menos de 20 é {quantmulheres}')         
     
 
 
--- python-mundo-2/exe-52.py
+++ python-mundo-2/exe-52.py
@@ -1,14 +1,33 @@
+
+opcoes = 0
+res = 0
 n1 = int(input('Digite o primeiro valor:'))
 n2 = int(input('Digite o segundo valor: '))
-total = 0
-numero = int(input('[1] somar [2] multiplicar [3] maior [4] novos números [5] sair do programa'))
-while numero == 5:
-    if numero == 1:
-        total += n1 + n2
-    if numero == multiplicar:
-        total += n1*n2
-        if  n1 > n2:
-            
+while opcoes != 5:
+    
 
+    print('''    [1] somar
+    [2] multiplicar
+    [3] maior
+    [4] novos números 
+    [5] sair do programa''')
 
-print('O programa fechou!')
+    opcoes = int(input('>>>Digite uma opção.'))
+    if opcoes == 1:
+        res = n1 + n2
+        print(f'A soma de {n1} + {n2} é {res} ')
+    elif opcoes == 2:
+        res = n1 * n2
+        print(f'A multiplicão de {n1} x {n2} é {res}')  
+    elif opcoes == 3:
+        if n1 > n2:
+            print(f'{n1} é maior que {n2}')
+        else:
+            print(f'{n2} é maior que {n1}') 
+    elif opcoes == 4:
+        n1 = int(input('Digite o primeiro valor:'))
+        n2 = int(input('Digite o segundo valor: '))
+    elif opcoes == 5:
+        print('finalizando...')
+
+print('O programa foi fechado.')
